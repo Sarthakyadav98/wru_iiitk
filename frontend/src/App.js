5d0798b6438ddeb8ae02e1fd37e8725655723e15
Sarthakyadav98 <s9808927657@gmail.com>
2026-03-12 14:14:31 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>React Frontend</h1>
+        <p>Welcome to your React application!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
